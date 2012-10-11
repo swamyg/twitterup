891b382cb155f7f5777f6506260744ed33f778f9
Swamy G <swamy@manilla.com>
2012-10-10 22:13:12 -0700

    First commit, installed rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitterup::Application.config.secret_token = '2c76bbd2f69811245df80e3c97f06ee3feb92fcb7ba81e89c2f10801fad0a223b57d7795cc17911036ea34d15287bc8eecf2e054503f4a1618c6258e6dfbd6dd'
